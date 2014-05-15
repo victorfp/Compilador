1a1667bf1527a855685372cccd174ff2a35e61ad
Victor Ferreira Pereira <victorferreirapereira@gmail.com>
2014-05-14 21:56:09 -0300

    Revert "Doc"
    This reverts commit 0394a61a9523386922433e4d273664ebc88fc4bc.

--- Compilador/Doc/script.bat
+++
@@ -1,7 +0,0 @@
-echo off
-echo Preparando para executar SableCC.
-rmdir /Q /S ..\src\compilador
-echo Executando SableCC
-java -jar .\sablecc.jar .\especificacao.txt
-move .\compilador ..\src
-pause
