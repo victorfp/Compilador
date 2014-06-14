0b90588eff21e352556793983d26e0c0153fe593
ThiagoMArc <tf.arcanjo@gmail.com>
2014-06-14 17:58:11 -0300

    Commit

--- compilador/Doc/script.bat
+++ compilador/Doc/script.bat
@@ -1,7 +1,20 @@
 echo off
+echo ============================================
 echo Preparando para executar SableCC.
+echo ============================================
+rmdir /Q /S .\compilador
 rmdir /Q /S ..\src\compilador
+echo ============================================
 echo Executando SableCC
+echo ============================================
 java -jar .\sablecc.jar .\especificacao.txt
-move .\compilador ..\src
+md ..\src\compilador
+echo ============================================
+echo Movendo arquivos para compilador\src
+echo ============================================
+xcopy /-Y /S .\compilador\* ..\src\compilador
+
+echo Execucao finalizada com sucesso
+
+rmdir /Q /S .\compilador
 pause
