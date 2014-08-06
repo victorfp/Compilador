ef0fa6469e8f40737a9426cde1cf47269f7e58ca
Victor Ferreira Pereira <victorferreirapereira@gmail.com>
2014-08-06 16:51:29 -0300

    commit final

--- compilador/Doc/compilador.bat
+++ compilador/Doc/compilador.bat
@@ -1,6 +1,6 @@
 @ECHO off
 ECHO Iniciando o Compilador SimplePortugol
-ECHO Iniciando Testes
 java -jar .\compilador.jar %1
-echo Sequencia de Testes Finalizados
+
+
 pause
--- compilador/src/main/Main.java
+++ compilador/src/main/Main.java
@@ -15,6 +15,7 @@ public class Main {
 		try {
 			
 			if (args.length > 0){
+				System.out.println("t: "+ args[0]);
 				for (int i = 0; i < args.length; i++) {
 					//System.out.println("Iniciando Teste para '"+args[i]+"'\n");
 					r = new FileReader(args[i]);
@@ -28,18 +29,20 @@ public class Main {
 				}
 			}
 			else{
-				String dir = "./Teste";
+				String dir = "../Teste";
 				File f = new File(dir);
-				String[] names = f.list();
-				for (int i = 0; i < names.length; i++) {
-					System.out.println("Iniciando Teste para '"+names[i]+"'\n");
-					r = new FileReader(dir+"/"+names[i]);
-				
-					Parser p = new Parser(new MyLexer(new PushbackReader(r,1024)));
-					Start s = p.parse();
-					s.apply(new SemanticAnalyzer());
-					System.out.println("\nTeste Finalizado com Sucesso\n");
-					r.close();
+				if (f.exists()){
+					String[] names = f.list();
+					for (int i = 0; i < names.length; i++) {
+						System.out.println("Iniciando Teste para '"+names[i]+"'\n");
+						r = new FileReader(dir+"/"+names[i]);
+					
+						Parser p = new Parser(new MyLexer(new PushbackReader(r,1024)));
+						Start s = p.parse();
+						s.apply(new SemanticAnalyzer());
+						System.out.println("\nTeste Finalizado com Sucesso\n");
+						r.close();
+					}
 				}
 			}
 		} catch (Exception e) {
